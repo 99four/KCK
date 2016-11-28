62d343ef42c0949062b6e7a8dd0bba40677eee36
Damian Bachórz <damian.bachorz@briisk.co>
2016-11-28 21:11:16 +0100

    2 plots

--- visualisation.py
+++ visualisation.py
@@ -2,12 +2,36 @@
 # -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
 
-def main():
-    plt.figure(figsize=(3, 3))
+def mean(numbers):
+    return float(sum([float(i) for i in numbers])) / len(numbers)
 
-    plt.plot([100,200,300,400],[0.1,0.2,0.8,0.9])
+def main():
+    efforts_2cel = []
+    efforts_2cel_rs = []
+    results_2cel = []
+    results_2cel_rs = []
+
+    with open('2cel.csv', 'r') as file_2cel, open('2cel-rs.csv', 'r') as file_2cel_rs:
+        lines_2cel = file_2cel.readlines()
+        lines_2cel_res = file_2cel_rs.readlines()
+
+    for i in range(1, len(lines_2cel)):
+        line_values = lines_2cel[i].split(',')
+        efforts_2cel.append(line_values[1])
+        # results_2cel.append()
+        results_2cel.append(mean(line_values[2:]))
+
+    for i in range(1, len(lines_2cel_res)):
+        line_values = lines_2cel_res[i].split(',')
+        efforts_2cel_rs.append(line_values[1])
+        # results.append()
+        results_2cel_rs.append(mean(line_values[2:]))
+
+    plt.figure(figsize=(6, 6))
+    #
+    plt.plot(efforts_2cel, results_2cel)
     plt.show()
-    plt.savefig('myplot.pdf')
+    # plt.savefig('myplot.pdf')
     plt.close()
 
 if __name__ == '__main__':
