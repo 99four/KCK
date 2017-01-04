7c84547fd8604bfa906b6c039146b76226fd5ad9
Damian Bachórz <damian.bachorz@briisk.co>
2017-01-04 01:18:52 +0100

    optimizing imports

--- images/images.py
+++ images/images.py
@@ -1,15 +1,13 @@
 from matplotlib import pyplot as plt
-from skimage import data, feature
-from skimage.filters import gaussian
-from skimage.morphology import dilation
+from skimage import data, feature, filters, morphology
 
 plt.figure(figsize=(9, 9))
 for i, file_name in enumerate(['samolot02.jpg', 'samolot03.jpg', 'samolot09.jpg', 'samolot12.jpg', 'samolot14.jpg',
                                'samolot16.jpg']):
     image = data.imread(file_name, True)
     image = feature.canny(image, sigma=3)
-    image = dilation(image)
-    image = gaussian(image, sigma=1)
+    image = morphology.dilation(image)
+    image = filters.gaussian(image, sigma=1)
     plt.subplot(3, 2, i + 1)
     plt.axis('off')
     plt.imshow(image, cmap="gray")
