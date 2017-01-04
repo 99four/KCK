e054fe1ae78e27b677e0384ebe7e4e04963f219e
Damian Bachórz <damian.bachorz@briisk.co>
2017-01-04 22:32:15 +0100

    fix

--- images/images.py
+++ images/images.py
@@ -2,12 +2,11 @@ from matplotlib import pyplot as plt
 from skimage import data, feature, filters, morphology
 
 plt.figure(figsize=(9, 9))
-for i, file_name in enumerate(['samolot02.jpg', 'samolot03.jpg', 'samolot09.jpg', 'samolot12.jpg', 'samolot14.jpg',
+for i, file_name in enumerate(['samolot01.jpg', 'samolot03.jpg', 'samolot17.jpg', 'samolot07.jpg', 'samolot14.jpg',
                                'samolot16.jpg']):
     image = data.imread(file_name, True)
     image = feature.canny(image, sigma=3)
-    image = morphology.dilation(image)
-    image = filters.gaussian(image, sigma=1)
+    image = morphology.dilation(image, morphology.disk(2))
     plt.subplot(3, 2, i + 1)
     plt.axis('off')
     plt.imshow(image, cmap="gray")
