cf8a3bfc7655ba17f428b938954eb81b501be469
Damian Bachórz <damian.bachorz@briisk.co>
2017-01-06 21:21:28 +0100

    hsv_to_rgb method from matplotlib

--- colors/colors.py
+++ colors/colors.py
@@ -41,15 +41,7 @@ def plot_color_gradients(gradients, names):
     fig.savefig('my-gradients.pdf')
 
 def hsv2rgb(h, s, v):
-    if s == 0.0: return [v, v, v]
-    i = int(h*6.)
-    f = (h*6.)-i; p,q,t = v*(1.-s), v*(1.-s*f), v*(1.-s*(1.-f)); i%=6
-    if i == 0: return [v, t, p]
-    if i == 1: return [q, v, p]
-    if i == 2: return [p, v, t]
-    if i == 3: return [p, q, v]
-    if i == 4: return [t, p, v]
-    if i == 5: return [v, p, q]
+    return colors.hsv_to_rgb([h, s, v])
 
 def gradient_rgb_bw(v):
     return (v, v, v)
@@ -119,7 +111,6 @@ def gradient_hsv_unknown(v):
 
 
 def gradient_hsv_custom(v):
-    #TODO
     return hsv2rgb(v, v, 1-v)
 
 
